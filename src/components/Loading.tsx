0d516b5f271d769a15e0d8f8b47cc459a199f1f1
Running water <alsgks1218@naver.com>
2023-02-06 23:25:57 +0900

    로딩페이지 및 기본세팅

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="container">
+      <div className="half red" />
+      <div className="half white" />
+      <div className="pale-black bond" />
+      <div className="ball-center" />
+      <div className="ball-center-pale-black" />
+      <div className="big-white-ball" />
+      <div className="small-white-ball" />
+    </div>
+  );
+};
+
+export default Loading;
