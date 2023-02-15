219f978f9ddaab7e265559e0264119370f81ff38
Running water <alsgks1218@naver.com>
2023-02-15 23:47:07 +0900

    api env에 저장

--- src/api/pokemon-card.api.ts
+++ src/api/pokemon-card.api.ts
@@ -2,7 +2,12 @@ import Axios from "axios";
 
 export const getPokemonCard = async () => {
   const { data } = await Axios.get(
-    "https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]"
+    "https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:[1 TO 151]",
+    {
+      headers: {
+        "X-Api-Key": process.env.REACT_APP_API,
+      },
+    }
   );
   return data;
 };
