5aae79237b19a5f8ccf3b4fb65a5024007e4a1cb
minhanPark <alsgks1218@naver.com>
2023-03-03 15:23:38 +0900

    회전기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { getPokemonCard, PokemonCard } from "./api/pokemon-card.api";
 import Loading from "./components/Loading";
 import Card from "./components/Card";
@@ -31,12 +31,33 @@ const Carousel = styled.div`
   transition: 1s;
 `;
 
+const ArrowContainer = styled.div`
+  position: fixed;
+  display: flex;
+  bottom: 0;
+  width: 100%;
+  height: 3.5rem;
+  background-color: #58585a;
+`;
+
+const ArrowWrapper = styled.div`
+  cursor: pointer;
+  width: 50%;
+  height: 100%;
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid white;
+`;
+
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const cellCount = 15;
   const theta = 360 / cellCount;
   const cellSize = 14.9;
   const radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount));
+  const carouselRef = useRef<HTMLDivElement>(null);
   const getRandomCardIndexes = (pokemonData: PokemonCard[]) => {
     let indexes: number[] = [];
     let randomPokemonData: PokemonCard[] = [];
@@ -49,6 +70,7 @@ function App() {
     }
     return randomPokemonData;
   };
+
   const { data } = useQuery({
     queryKey: ["pokemonCard"],
     queryFn: () => getPokemonCard(),
@@ -59,21 +81,83 @@ function App() {
       };
     },
   });
+  const rotateCarousel = (selectedIndex: number) => {
+    const angle = theta * selectedIndex * -1;
+    if (carouselRef?.current) {
+      carouselRef.current.style.transform = `translateZ(${-radius}rem) rotateY(${angle}deg)`;
+    }
+  };
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => {
+      rotateCarousel(prevIndex - 1);
+      return prevIndex - 1;
+    });
+  };
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => {
+      rotateCarousel(prevIndex + 1);
+      return prevIndex + 1;
+    });
+  };
   console.log(data);
   return (
-    <Container>
-      <CarouselScene>
-        <Carousel>
-          {data?.data.map((card) => (
-            <Card
-              key={`${card.id}-${card.name}`}
-              name={card.name}
-              image={card.images.large}
+    <>
+      <Container>
+        <CarouselScene>
+          <Carousel ref={carouselRef}>
+            {data?.data.map((card) => (
+              <Card
+                key={`${card.id}-${card.name}`}
+                name={card.name}
+                image={card.images.large}
+              />
+            ))}
+          </Carousel>
+        </CarouselScene>
+      </Container>
+      <ArrowContainer>
+        <ArrowWrapper onClick={handlePrev}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            style={{
+              width: "48px",
+              height: "48px",
+              color: "white",
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
             />
-          ))}
-        </Carousel>
-      </CarouselScene>
-    </Container>
+          </svg>
+        </ArrowWrapper>
+        <ArrowWrapper onClick={handleNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            style={{
+              width: "48px",
+              height: "48px",
+              color: "white",
+            }}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+            />
+          </svg>
+        </ArrowWrapper>
+      </ArrowContainer>
+    </>
   );
 }
 
